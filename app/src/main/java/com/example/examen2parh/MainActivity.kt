a864457a482e356a34687662a26953e4ab84b18a
Pablo Rojas <123708990+pablo-rojas-dev@users.noreply.github.com>
2026-05-27 21:06:03 -0600

    Validación de contraseña

--- app/src/main/java/com/example/examen2parh/MainActivity.kt
+++ app/src/main/java/com/example/examen2parh/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.examen2parh
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.examen2parh.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        iniciarSesion()
+    }
+
+
+    private fun iniciarSesion(){
+        binding.btnInciarSesion.setOnClickListener {
+
+            val contrasenia = "abc123"
+
+            if (binding.etContrasenia.text.toString() != contrasenia) {
+                Toast.makeText(this, "Contraseña incorrecta",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }else{
+                val intent = Intent(this, ProfileActivity::class.java).apply{
+
+                }
+            }
+
+            startActivity(intent)
+
         }
     }
 }
